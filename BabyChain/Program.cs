4668237845291fc2771c7289f18986c129b682b8
mustafakurban <mustafakurban155@gmail.com>
2024-12-30 17:09:10 +0300

    Add Node class

--- BabyChain/Program.cs
+++ BabyChain/Program.cs
@@ -1,31 +1,26 @@
 ﻿using System;
 
-namespace BlockchainDemo
+
+namespace BabyChainDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Blockchain blockchain = new Blockchain();
+            Node node1 = new("Node1");
+            Node node2 = new("Node2");
 
-            blockchain.AddBlock(new Block(1, DateTime.Now, "Block 1 Data", ""));
-            blockchain.AddBlock(new Block(2, DateTime.Now, "Block 2 Data", ""));
-            blockchain.AddBlock(new Block(3, DateTime.Now, "Block 3 Data", ""));
-            blockchain.AddBlock(new Block(4, DateTime.Now, "Block 4 Data", ""));
-            blockchain.AddBlock(new Block(5, DateTime.Now, "Block 5 Data", ""));
-            blockchain.AddBlock(new Block(6, DateTime.Now, "Block 6 Data", ""));
-            blockchain.AddBlock(new Block(7, DateTime.Now, "Block 7 Data", ""));
-            blockchain.AddBlock(new Block(8, DateTime.Now, "Block 8 Data", ""));
+            node1.StartListening(5000);
+            node2.StartListening(5001);
 
-            foreach (var block in blockchain.Chain)
-            {
-                Console.WriteLine($"Index: {block.Index}");
-                Console.WriteLine($"Timestamp: {block.Timestamp}");
-                Console.WriteLine($"Data: {block.Data}");
-                Console.WriteLine($"Previous Hash: {block.PreviousHash}");
-                Console.WriteLine($"Hash: {block.Hash}");
-                Console.WriteLine();
-            }
+            node1.ConnectToPeer("127.0.0.1:5001");
+            node2.ConnectToPeer("127.0.0.1:5000");
+
+            node1.Blockchain.AddBlock(new Block(1, DateTime.Now, "Block 1 Data", ""));
+            node1.Broadcast("BLOCK:" + "Block 1 Data");
+
+            Console.ReadLine();
         }
+
     }
 }
